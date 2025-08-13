648a92ded94a74be2bc297792f7945ca55839d8e
EL-jos <elongajosue22@gmail.com>
2025-08-13 17:48:28 +0100

    Premier commit

---
+++ main.py
@@ -0,0 +1,94 @@
+import os
+from urllib.error import HTTPError
+
+import pandas as pd
+import numpy as np
+import matplotlib
+
+# Choix du backend AVANT d'importer pyplot
+matplotlib.use('TkAgg')
+
+import matplotlib.pyplot as plt
+
+from chp2.functions import plot_decision_regions
+
+from chp2.perceptron import Perceptron
+from chp2.Adaline import Adaline
+
+try:
+    s = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+    print('From URL:', s)
+    df = pd.read_csv(s, header=None, encoding='utf-8')
+except HTTPError:
+    s = 'chp2/iris.csv'
+    print('From local Iris path:', s)
+    df = pd.read_csv(s, header=None, encoding='utf-8')
+
+print(df.tail())
+
+# select setosa and versicolor
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', 0, 1)
+
+# extract sepal length and petal length
+X = df.iloc[0:100, [0, 2]].values
+"""
+# plot data
+plt.scatter(X[:50, 0], X[:50, 1],
+            color='red', marker='o', label='Setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1],
+            color='blue', marker='s', label='Versicolor')
+
+plt.xlabel('Sepal length [cm]')
+plt.ylabel('Petal length [cm]')
+plt.legend(loc='upper left')
+"""
+"""
+ppn = Perceptron(eta=0.1, n_iter=10)
+
+ppn.fit(X, y)
+
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel('Sepal length [cm]')
+plt.ylabel('Petal length [cm]')
+plt.legend(loc='upper left')
+"""
+"""
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+
+ada1 = Adaline(n_iter=15, eta=0.1).fit(X, y)
+ax[0].plot(range(1, len(ada1.losses_) + 1), np.log10(ada1.losses_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Mean squared error)')
+ax[0].set_title('Adaline - Learning rate 0.1')
+
+ada2 = Adaline(n_iter=15, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.losses_) + 1), ada2.losses_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Mean squared error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+"""
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+ada_gd = Adaline(n_iter=20, eta=0.5)
+ada_gd.fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier=ada_gd)
+plt.title('Adaline - Gradient descent')
+plt.xlabel('Sepal length [standardized]')
+plt.ylabel('Petal length [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+#plt.savefig('images/02_14_1.png', dpi=300)
+plt.show()
+
+plt.plot(range(1, len(ada_gd.losses_) + 1), ada_gd.losses_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Mean squared error')
+
+plt.tight_layout()
+
+#plt.savefig('images/02_08.png', dpi=300)
+plt.show()
